0c29569e5833f11a90ef359a437d5d3ebadcec55
balamurugansp <spbalamurugan@gmail.com>
2017-06-12 11:24:50 +0530

    Create App.java

---
+++ src/main/java/com/aconex/constants/App.java
@@ -0,0 +1,13 @@
+package com.aconex.constants;
+
+public class App {
+    public App() {/*Constructor for code coverage*/}
+
+    public static final String INPUT_VALIDATION_REGEX = "^[AaBb][0-9]+$";
+    public static final String SENSOR1_PREFIX = "A";
+    public static final String SENSOR2_PREFIX = "B";
+    public static final double LENGTH_OF_VEHICLE = 0.0025; //in km
+    public static final int NUMBER_OF_DAYS = 5;
+    public static final double AVERAGE_SPEED = 16.6667; // in m/s
+    public static final String DEFAULT_INPUT_FILE_PATH = "data.txt";
+}
