d00e10cd8712a53afd3a27ab616000c778ebdb5f
balamurugansp <spbalamurugan@gmail.com>
2017-06-12 11:26:23 +0530

    Create TimeParser.java

---
+++ src/main/java/com/aconex/helpers/TimeParser.java
@@ -0,0 +1,27 @@
+package com.aconex.helpers;
+
+import com.aconex.constants.Time;
+
+public class TimeParser {
+
+    public static int parseMilliSecondFromInput(String input){
+        try {
+            int milliSeconds = Integer.parseInt(input.substring(1));
+            if (milliSeconds < 0 )
+                return -1;
+            return milliSeconds;
+        }catch (NumberFormatException e){
+            System.out.println("Error parsing time from : " + input);
+        }
+        return -1;
+    }
+
+    public static double convertToHour(int milliseconds){
+        if (milliseconds <0 )
+            return -1;
+        double minutesInHour = Time.MINUTES_IN_A_HOUR;
+        double secondsInMinute = Time.SECONDS_IN_A_MINUTE;
+        double milliSecondsInSecond = Time.MILLISECONDS_IN_A_SECOND;
+        return ((double)milliseconds)/(milliSecondsInSecond * secondsInMinute * minutesInHour);
+    }
+}
